ddb71be0f8afc9ca06c9268c57113af1393c7de9
Davy Moreau <sz8194@equans.com>
2025-05-23 15:59:48 +0200

    Ajout des repository (non testés)

---
+++ Domain/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Repository
+{    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+        Task SaveChangeAsync();
+    }
+}
---
+++ Infrastructure/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        internal MVCOnionContext context;
+        internal DbSet<TEntity> dbSet;
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
